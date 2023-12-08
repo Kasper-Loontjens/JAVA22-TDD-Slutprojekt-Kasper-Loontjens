05e8daf9ac8c4e69b57e973eedfc6af1e19ba2f7
Kasper-Loontjens <112395890+Kasper-Loontjens@users.noreply.github.com>
2023-12-08 12:50:31 +0100

    Add files via upload

--- tddSlutProjektKasper/src/test/BufferTest.java
+++ tddSlutProjektKasper/src/test/BufferTest.java
@@ -153,48 +153,5 @@ public class BufferTest {
 
 	}
 	
-	// item tests 
 	
-	@Test
-	@RepeatedTest(1)
-	@DisplayName("all uppercase letters should be returned as lowercase")
-	public void itemToStringUpperToLowerTest() {
-		Item item = new Item("ITEM");
-		assertEquals("item", item.toString());
-	}
-	
-	@Test
-	@RepeatedTest(1)
-	@DisplayName("setId Test")
-	public void setIdTest() {
-		Item item = new Item("ITEM");
-		item.setId("HanD Bag");
-		assertEquals("hand bag", item.toString());
-	}
-	
-	@Test
-	@RepeatedTest(1)
-	@DisplayName("Item id is null test ")
-	public void itemIdIsNullTest() {
-		
-		Item item = new Item(null);
-		assertNull(item.toString() );
-		assertNull(new Item(null));
-		
-		// When id is null no function works but the item is also neither null nor does it throw any exception
-	}
-	
-	@Test
-	@RepeatedTest(1)
-	@DisplayName("Item setId is null test ")
-	public void itemSetIdIsNullTest() {
-		
-		Item item = new Item("test");
-		item.setId(null);
-		assertNotNull(item.toString());
-		assertEquals("test", item.toString());
-		assertNull(item.toString());
-		
-		// When id is set with null an error occurs, no Try/catch.
-	}
 }
